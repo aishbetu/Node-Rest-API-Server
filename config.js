c8dd1261c2895b4f459a603dfad2913ea200122c
unknown <aishbetu@gmail.com>
2021-10-07 16:40:02 +0530

    passport facebook oAuth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
-    'secretkey' : '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '342410917658637',
+        clientSecret: '0d075bc6aee94bb9e154867627871a61'
+    }
 }
